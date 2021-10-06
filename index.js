cf3f2e2f1bba5445c94916a9e3f3bb13791443e5
Scott Davies <scottdavies1983@gmail.com>
2021-10-06 06:07:38 +0100

    completed 3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/info', (request, response) => {
+  const date = new Date();
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>`)
+})
+
 // app.get('/api/notes/:id', (request, response) => {
 //   const id = Number(request.params.id)
 //   const note = notes.find(note => note.id === id)
