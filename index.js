ca62fe3600e00f1f71a02ff6c9e86e3d1f3ba18a
Scott Davies <scottdavies1983@gmail.com>
2021-10-26 08:38:19 +0100

    added basic error handling middleware 3.16: Phonebook database, step4

--- index.js
+++ index.js
@@ -75,9 +75,7 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-// app.get('/api/persons', (request, response) => {
-//   response.json(persons)
-// })
+/
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -86,78 +84,18 @@ app.get('/api/persons', (request, response) => {
 })
 
 
-// app.get('/api/persons/:id', (request, response) => {
-//   const id = Number(request.params.id)
-//   const person = persons.find(note => note.id === id)
-  
-//   if (person) {
-//     response.json(person)
-//   } else {
-//     response.status(404).end()
-//   }
-// })
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-
-// app.get('/api/info', (request, response) => {
-//   const date = new Date();
-//   response.send(`
-//     <p>Phonebook has info for ${persons.length} people</p>
-//     <p>${date}</p>`)
-// })
-
-
-// app.delete('/api/persons/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     const person = persons.find(note => note.id === id)
-//     persons = persons.filter(person => person.id !== id)
-
-//     //If an entry for the given id is not found, the server has to respond with the appropriate status code *ADDED THIS FROM WRONG TASK BUT IT WORKS SO...
-//     if (person) {
-//       response.status(204).end()
-//     } else {
-//       response.status(404).end()
-//     }
-//   })
-
-//   const generateId = () => {
-//     const maxId = persons.length > 0
-//       ? Math.max(...persons.map(n => n.id))
-//       : 0
-//     return maxId + 1
-//   }
-  
-  // app.post('/api/persons', (request, response) => {
-  //   const body = request.body
-  //   //Could improve this with case comparison
-  //   const newPerson = persons.find(person => person.name === body.name)
-  
-  //   if (!body.name || !body.number) {
-  //     return response.status(400).json({ 
-  //       error: 'information missing' 
-  //     })
-  //   }
-
-  //   if (newPerson) {
-  //     return response.status(400).json({ 
-  //       error: 'person already exists' 
-  //     })
-  //   }
-  
-  //   const person = {
-  //     id: generateId(),
-  //     name: body.name,
-  //     number: body.number,
-  //   }
-  
-  //   persons = persons.concat(person)
-  
-  //   response.json(person)
-  // })
 
   app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -191,6 +129,18 @@ app.get('/api/persons/:id', (request, response) => {
   
   app.use(unknownEndpoint);
 
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+    //In all other error situations, the middleware passes the error forward to the default Express error handler.
+    next(error)
+  }
+  
+  // this has to be the last loaded middleware.
+  app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
